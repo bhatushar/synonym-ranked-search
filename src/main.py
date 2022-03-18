fd0c26a062cac5f7e8a870d93d8dbd91b189df9c
Tushar Bhatt <tbhatt321@gmail.com>
2022-03-18 16:21:45 +0530

    Refactor, add utilities and create driver code

--- src/main.py
+++ src/main.py
@@ -1,13 +1,31 @@
-import os
-from libs import startup
+import pandas as pd
+from libs import startup, search
 
 
 if __name__ == "__main__":
-    os.makedirs("data/docs", exist_ok=True)
-    os.makedirs("data/encrypted", exist_ok=True)
-    os.makedirs("data/meta", exist_ok=True)
-
+    print("Starting server...")
+    startup.mkdirs()
     print("Computing metadata: TF and IFD")
     startup.compute_metadata()
     print("Encrypting documents")
     startup.encrypt_docs()
+
+    try:
+        while True:
+            query = input("Search> ")
+            results = search.find_docs(query)
+            results_df = pd.DataFrame(results, columns=["Document", "Score"])
+            print(results_df)
+            while True:
+                doc_id = input("Open (q to exit)> ")
+                if doc_id == "q":
+                    break
+                doc = results_df.iloc[int(doc_id)]["Document"]
+                # Decrypt document and open with webbrowser
+                print("Opening ", doc)
+                startup.open_doc(doc)
+    except KeyboardInterrupt:
+        print()
+        print("Shutting down server...")
+        startup.cleanup()
+        print("Goodbye!")
