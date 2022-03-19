0caa6f1b3b9f0d0ae169e1ae00209fde51b62876
Tushar Bhatt <tbhatt321@gmail.com>
2022-03-19 23:07:08 +0530

    Calculating average search time

--- src/run-tests.py
+++ src/run-tests.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import time
 from libs import env, search, system
 
 
@@ -10,11 +11,13 @@ if __name__ == "__main__":
     system.encrypt_docs()
     df = pd.read_csv(env.QUERIES_PATH)
     rank_dev = 0
+    duration = 0
     for _, row in df.iterrows():
         file = row["Files"]
         query = row["Queries"]
+        start = time.perf_counter()
         results = search.find_docs(query)
-
+        duration += time.perf_counter() - start
         rank = -1
         for search_file, score in results:
             rank += 1
@@ -23,4 +26,6 @@ if __name__ == "__main__":
         rank_dev += rank**2
     tests = len(df.index)
     mse = rank_dev / tests
-    print("Mean Squared Error: ", mse)
+    avg_time = duration / tests
+    print("Average search time:", avg_time, "sec")
+    print("Mean Squared Error:", mse)
